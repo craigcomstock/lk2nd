ef375b8eebbf94d3d6e002dbf8ebbfbf5f03c054
Craig Comstock <craig@unreasonablefarm.org>
2020-07-08 08:28:06 -0500

    wip, some stuff I didnt want to lose

--- dts/rules.mk
+++ dts/rules.mk
@@ -5,8 +5,17 @@ DTBS += \
 	$(LOCAL_DIR)/apq8016-samsung-r02.dtb \
 	$(LOCAL_DIR)/apq8016-samsung-r07.dtb \
 	$(LOCAL_DIR)/msm8216-samsung-r05.dtb \
-	$(LOCAL_DIR)/msm8916-motorola-harpia.dtb \
-	$(LOCAL_DIR)/msm8916-motorola-surnia.dtb \
+	$(LOCAL_DIR)/msm8916-motorola-surnia-p1ad.dtb \
+	$(LOCAL_DIR)/msm8916-motorola-surnia-p1a.dtb \
+	$(LOCAL_DIR)/msm8916-motorola-surnia-p1bd.dtb \
+	$(LOCAL_DIR)/msm8916-motorola-surnia-p1b.dtb \
+	$(LOCAL_DIR)/msm8916-motorola-surnia-p2ad.dtb \
+	$(LOCAL_DIR)/msm8916-motorola-surnia-p2a.dtb \
+	$(LOCAL_DIR)/msm8916-motorola-surnia-p2bd.dtb \
+	$(LOCAL_DIR)/msm8916-motorola-surnia-p2b.dtb \
+	$(LOCAL_DIR)/msm8916-motorola-surnia-p3d.dtb \
+	$(LOCAL_DIR)/msm8916-motorola-surnia-p3.dtb \
+	$(LOCAL_DIR)/msm8916-motorola-surnia-all.dtb \
 	$(LOCAL_DIR)/msm8916-mtp.dtb \
 	$(LOCAL_DIR)/msm8916-qrd9-v1.dtb \
 	$(LOCAL_DIR)/msm8916-samsung-r01.dtb \
